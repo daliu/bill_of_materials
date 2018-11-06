ddda9159189eafa8ad2b1c9d9f6a789bd39197ab
Dave Liu <daveliu@Daves-MacBook-Pro.local>
2018-11-06 00:42:23 -0800

    Updated readme and fixed GET functions. Added comments, restructured BoM object, and added more test cases. Also added abstraction barrier functions. Removed Models.py because unecessary.

--- README.md
+++ README.md
@@ -5,14 +5,16 @@ Make sure you have Python3, pip, and virtualenv
 > pip install -r requirements.txt
 > python app.py (Server should be running on localhost:5000)
 
+## Note: When using curl, make sure URLs always end in backslash
+
+
 # To Create a Part object:
-(Must specify correct API route, header, and data columns, id will be automatically specified.)
+(Must specify correct API route, header, and at least a name in data dictionary. ID will be automatically specified.)
 > curl -X POST http://localhost:5000/create/ --header "Content-Type: application/json" -d '{"name": "phone", "color":"silver", "model": "iPhone"}'
 
 
 # To Update a Part Object:
-> curl -X PUT http://localhost:5000/update/<int:part_id>/ --header "Content-Type: application/json" -d 'DATA_DICTIONARY'
-E.g. DATA_DICTIONARY might look like '{"color": "orange"}'
+> curl -X PUT http://localhost:5000/update/<int:part_id>/ --header "Content-Type: application/json" -d '{"color": "orange"}'
 
 
 # To Delete a Part Object:
@@ -22,32 +24,26 @@ E.g. DATA_DICTIONARY might look like '{"color": "orange"}'
 
 
 # Listing Parts:
-
 ### To list all parts in BoM:
 > curl -X GET http://localhost:5000/bom/
 
 ### To list all assemblies:
-> curl -X GET http://localhost:5000/
+> curl -X GET http://localhost:5000/assemblies/
 
 ### To list all top level assemblies (assemblies that are not children of another assembly):
 > curl -X GET http://localhost:5000/toplevel/
 
-
 ### To list all subassemblies (assemblies that are children of another assembly):
 > curl -X GET http://localhost:5000/subassems/
-
 
 ### To list all component parts (parts that are not subassemblies, but are included in a parent assembly):
 > curl -X GET http://localhost:5000/components/
 
-
 ### To list all orphan parts (parts with neither parents nor children):
 > curl -X GET http://localhost:5000/orphans/
 
-
 ### To list all the first-level children of a specific assembly:
-> curl -X GET http://localhost:5000/first_children/
-
+> curl -X GET http://localhost:5000/assembly/children/<int: ID>
 
 ### To list all parts in a specific assembly:
 > curl -X GET http://localhost:5000/parts/ID/
@@ -57,8 +53,6 @@ E.g. DATA_DICTIONARY might look like '{"color": "orange"}'
 
 
 # Future Improvements:
-##### Instead of storing as a list, I should have used a dictionary of ID -> Obj in order to increase speed of access
-##### Additionally, we should store static values until we add/update/delete values, at which point only then would we need to make updates to our relational "database".
+##### We should store static values until we add/update/delete values, at which point only then would we need to make updates to our relational "database".
 ##### Ultimately though, if we really wanted to be efficient, using a database management system would likely abstract-away solutions to our inefficient accessors and checking methods.
 ##### If we cannot abstract-away accessors to be more efficient, then at least we can take inspiration from common database management algorithms.
-##### I didn't have enough time to implement these efficiency improvements, but they are blatant weaknesses and straightforward to implement.
--- app.py
+++ app.py
@@ -1,71 +1,114 @@
 #!venv/bin/python
 from flask import Flask, request, abort, jsonify
-# from models import Assembly, Part
 import json
 import requests
 app = Flask(__name__)
 
 # List of physical parts
-bills_of_materials = [
-    {
+bill_of_mats = {
+    1: {
         'id': 1,
         'name': 'pen assembly',
         'color': 'red',
         'model': 'metal barrel',
-        'parent_id': None,
-        'children': [],
+        'parent_ids': None,
+        'children': [5, 8],
         'is_subassembly': False,
     },
-    {
+    2: {
         'id': 2,
         'name': 'pen assembly',
         'color': 'blue',
         'model': 'plastic barrel',
-        'parent_id': None,
-        'children': [],
+        'parent_ids': None,
+        'children': [5, 8],
         'is_subassembly': False,
     },
-    {
+    3: {
         'id': 3,
         'name': 'pen assembly',
         'color': 'green',
         'model': 'plastic barrel',
-        'parent_id': None,
-        'children': [],
+        'parent_ids': None,
+        'children': [5, 8],
         'is_subassembly': False,
     },
-    {
+    4: {
         'id': 4,
         'name': 'pen assembly',
         'color': 'gold',
-        'model': 'plastic barrel',
-        'parent_id': None,
-        'children': [5,6],
+        'model': 'metal barrel',
+        'parent_ids': None,
+        'children': [5, 8],
     },
-    {
+    5: {
         'id': 5,
         'name': 'top barrel assembly',
         'color': 'gold',
         'model': 'plastic barrel',
-        'parent_id': None,
-        'children': [],
-        'is_subassembly': False,
+        'parent_ids': 4,
+        'children': [6],
+        'is_subassembly': True,
     },
-    {
+    6: {
         'id': 6,
         'name': 'pocket clip',
         'color': 'gold',
         'model': 'plastic barrel',
-        'parent_id': 5,
+        'parent_ids': 5,
         'children': [],
+        'is_subassembly': False,
+    },
+    7: {
+        'id': 7,
+        'name': 'iPhone',
+        'color': 'gold',
+        'model': 'X',
+        'parent_ids': None,
+        'children': [],
+        'is_subassembly': False,
+    },
+    8: {
+        'id': 8,
+        'name': 'bottom barrel assembly',
+        'color': 'grey',
+        'model': 'polished wood',
+        'parent_ids': None,
+        'children': [9],
         'is_subassembly': True,
     },
+    9: {
+        'id': 9,
+        'name': 'rubber grip',
+        'color': 'gold',
+        'model': 'vynaprene',
+        'parent_ids': 8,
+        'children': [],
+        'is_subassembly': False,
+    },
     
-    
-]
+}
 
 def find_part(part_id):
-    return [part for part in bills_of_materials if part['id'] == part_id]
+    """
+    This function is defined as an abstraction barrier,
+    in case we want to use a different storage object for
+    BoM, such as perhaps a read/write database.
+
+    :part_id: the ID of the part specified
+    :return: part object specified by part_id, else None if DNE
+    """
+    return bill_of_mats.get(part_id, None)
+
+def get_bom_max_id():
+    """
+    This function is defined as an abstraction barrier,
+    minimizing the necessary changes if bill_of_mats becomes
+    a different type object.
+
+    :return: max ID of "database"
+    """
+    return max(bill_of_mats.keys())
 
 @app.route('/')
 def index():
@@ -74,66 +117,90 @@ def index():
 ### POST Routes ###
 @app.route('/create/', methods=['POST'])
 def create_part():
-    if not request.json:
-        print("Not JSON")
+    """
+    Creating an Assembly JSON object requires at least a name.
+    # Name is the only requirement in the JSON, for creating a new Assembly Object.
+    # We arbitrarily default to the color "black" and model "default".
+    """
+
+    if not request.json or 'name' not in request.json:
         abort(400)
 
-    for column in ['name', 'color', 'model']:
-        if column not in request.json:
-            print(column + " not in json")
-            abort(400)
-
-    if 'parent_id' not in request.json:
-        request.json['parent_id'] = None
+    if 'color' not in request.json:
+        request.json['color'] = 'black'
+    if 'model' not in request.json:
+        request.json['model'] = 'default'
+    if 'parent_ids' not in request.json:
+        request.json['parent_ids'] = None
     if 'children' not in request.json:
         request.json['children'] = []
 
+    
+    new_id = get_bom_max_id(bill_of_mats) + 1
     part = {
-        'id': bills_of_materials[-1]['id'] + 1,
+        'id': new_id,
         'name': request.json['name'],
         'color': request.json['color'],
         'model': request.json['model'],
-        'parent_id': request.json['parent_id'],
+        'parent_ids': request.json['parent_ids'],
         'children': request.json['children'],
         'is_subassembly': False,
         }
-    bills_of_materials.append(part)
+    bill_of_mats[new_id] = part
     return jsonify(part), 201
 
 ### DELETE Routes ###
 @app.route('/delete/<int:part_id>/', methods=['DELETE'])
 def delete_part(part_id):
-    delete_part = find_part(part_id)
-    if len(delete_part) == 0:
+    """
+    """
+    if find_part(part_id) is None:
         abort(404)
-    bills_of_materials.remove(delete_part[0])
-    return jsonify({'result': True})
+    result = bill_of_mats.pop(part_id)
+    return jsonify({'Deleted Result': result})
 
 ### PUT (Update) Routes ###
 @app.route('/update/<int:part_id>/', methods=['PUT'])
 def update_part(part_id):
-    old_part_lst = find_part(part_id)
-    if len(old_part_lst) != 1 or not request.json:
-        print(request.json, old_part_lst)
+    """
+    """
+    old_part = find_part(part_id)
+    if old_part is None or not request.json:
         abort(404)
 
-    old_part = old_part_lst[0]
-    bills_of_materials.remove(old_part)
+    # Updating parent_id and children requires modifying/updating their dependencies.
+    # 1) Check that parent ID(s) is/are valid
+    # 2) Add the current part to the parent's children
+    if 'parent_ids' in request.json:
+        new_parent_ids = request.json['parent_ids']
+        for parent_id in new_parent_ids:
+            if parent_id is None:
+                abort(404) # We abort instead of "pass"ing to signify an issue. Consider "pass" for convenience.
+            parent = find_part(parent_id)
+            parent['children'].append(part_id)
+    
+    # Updating children means either adding or removing some dependencies
+    # All children (add or remove) will need to update their parent id.
+    if 'children' in request.json:
+        deleted_children = [child_id for child_id in old_part['children'] if child_id not in request.json['children']]
+        for child_id in deleted_children:
+            find_part(child_id)['parent_ids'] = None
+        
+        new_children = [child_id for child_id in request.json['children'] if child_id not in old_part['children']]
+        for child_id in new_children:
+            find_part(child_id)['parent_ids'].append(part_id)
 
-    update_part = {
-        'id': old_part['id'],
+    updated_part = {
+        'id': part_id,
         'name': request.json['name'] if 'name' in request.json else old_part['name'],
         'color': request.json['color'] if 'color' in request.json else old_part['color'],
         'model': request.json['model'] if 'model' in request.json else old_part['model'],
-        'parent_id': request.json['parent_id'] if 'parent_id' in request.json else old_part['parent_id'],
+        'parent_ids': request.json['parent_ids'] if 'parent_ids' in request.json else old_part['parent_ids'],
         'children': request.json['children'] if 'children' in request.json else old_part['children'],
         'is_subassembly': request.json['is_subassembly'] if 'is_subassembly' in request.json else old_part['is_subassembly'],
     }
 
-    # TODO
-    # Update parent_id and children requires modifying/updating their dependencies.
-
-    bills_of_materials.append(update_part)
+    bill_of_mats[part_id] = updated_part
     return jsonify({'result': True})
 
 
@@ -143,7 +210,7 @@ def get_bom():
     """
     Returns jsonified list of Bill of Materials (BoM)
     """
-    return jsonify({'bills_of_materials': bills_of_materials}), 201
+    return jsonify({'bill_of_mats': bill_of_mats}), 201
 
 @app.route('/assemblies/', methods=['GET'])
 def get_assemblies():
@@ -152,8 +219,9 @@ def get_assemblies():
     Returns jsonified list of assemblies
     """
     assemblies = []
-    for part in bills_of_materials:
-        if part["children"] != []:
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['children'] != []:
             assemblies.append(part)
     return jsonify({'assemblies': assemblies}), 201
 
@@ -164,8 +232,9 @@ def get_toplevel():
     Returns jsonified list of top-level assemblies
     """
     top_assems = []
-    for part in bills_of_materials:
-        if part["parent_id"] is None and part["children"] != []:
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['parent_ids'] is None and part['children'] != []:
             top_assems.append(part)
     return jsonify({'top_level_assemblies': top_assems}), 201
 
@@ -176,8 +245,9 @@ def get_subassems():
     Returns jsonified list of subassems
     """
     subassems = []
-    for part in bills_of_materials:
-        if part["parent_id"] is not None and part["children"] != []:
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['is_subassembly']:
             subassems.append(part)
     return jsonify({'sub_assemblies': subassems}), 201
 
@@ -188,9 +258,10 @@ def get_components():
     Returns jsonified list of components
     """
     components = []
-    for part in bills_of_materials:
-        if part["parent_id"] is not None and find_part("parent_id")["parent_id"] is None:
-            components.append(part)
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['parent_ids'] and not part['is_subassembly']:
+            components.append(bill_of_mats[part_id])
     return jsonify({'components': components}), 201
 
 @app.route('/orphans/', methods=['GET'])
@@ -200,8 +271,9 @@ def get_orphans():
     Returns jsonified list of orphans
     """
     orphan_parts = []
-    for part in bills_of_materials:
-        if part['parent_id'] is None and part['children'] == []:
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['parent_ids'] is None and part['children'] == []:
             orphan_parts.append(part)
     return jsonify({'orphan_parts': orphan_parts}), 201
 
@@ -212,12 +284,11 @@ def get_first_children(assembly_id):
     Returns jsonified list of first_children
     """
     first_children = []
-    for part in bills_of_materials:
-        parent_id = part['parent_id']
-        if parent_id is not None and find_part(parent_id)['parent_id'] is None:
+    for part_id in bill_of_mats:
+        part = find_part(part_id)
+        if part['parent_ids'] == assembly_id:
             first_children.append(part)
     return jsonify({'first_level_children': first_children}), 201
-
 
 @app.route('/parts/<int:part_id>/', methods=['GET'])
 def get_part_by_id(part_id):
@@ -225,12 +296,7 @@ def get_part_by_id(part_id):
     Gets Part by ID number
     Returns jsonified list of part information.
     """
-    selected_part = []
-    for part in bills_of_materials:
-        if part["children"] != []:
-            selected_part.append(part)
-    return jsonify({'Part ' + str(part_id): selected_part}), 201
-
+    return jsonify({'Part ' + str(part_id): find_part(part_id)}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
